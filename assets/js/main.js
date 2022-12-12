bcda3da8a2ef260d9ba68906e4b350a958143db3
Christian Jaraula <qcjlljaraula@tip.edu.ph>
2022-12-12 21:51:46 +0800

    initial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 
 (function() {
   "use strict";
+  
+  var loader = document.querySelector("preloader")
+
+  window.addEventListener("load", vanish);
+
+  function vanish() {
+    loader.classList.add("disappear")
+  }
+  
 
 
   /**
